adc3aaaa2cc340a1f24d35a922e71a3ae2426550
hieudn0407 <doanngochieucdth15c.041097@gmail.com>
2026-04-18 09:46:13 +0700

    push

--- server-taixiu-4in1/config/database.js
+++ server-taixiu-4in1/config/database.js
@@ -1,25 +1,25 @@
 
-//module.exports = {
-//	'url': 'mongodb://127.0.0.1:27017',
-//	'options': {
-//		'user':   'root',
-//		'pass':   '',
-//		'dbName': 'PlayGameChuan2',
-//		'useNewUrlParser': true,
-//		'useUnifiedTopology': true,
-//		//'autoIndex':       false,
-//	},
-//};
-
-
 module.exports = {
-	'url': 'mongodb://168.144.35.114:27017',
+	'url': 'mongodb://127.0.0.1:27017',
 	'options': {
-		'user':   'root',
-		'pass':   '123456',
+		//'user':   'root',
+		//'pass':   '',
 		'dbName': 'PlayGame',
 		'useNewUrlParser': true,
 		'useUnifiedTopology': true,
 		//'autoIndex':       false,
 	},
 };
+
+
+// module.exports = {
+	// 'url': 'mongodb://168.144.35.114:27017',
+	// 'options': {
+		// 'user':   'root',
+		// 'pass':   '123456',
+		// 'dbName': 'PlayGame',
+		// 'useNewUrlParser': true,
+		// 'useUnifiedTopology': true,
+		// //'autoIndex':       false,
+	// },
+// };
